d7c8e32961739a443bfee8c25cee2474cc381f0a
Marius Schulz <marius.schulz@me.com>
2015-09-05 12:43:24 +0200

    Adds mocha, chai, and two simples tests

---
+++ test/iife.js
@@ -0,0 +1,14 @@
+var assert = require("chai").assert;
+var iife = require("../iife");
+
+describe("IIFE", function() {
+    describe("#surround()", function() {
+        it("should be a function", function() {
+            assert.typeOf(iife.surround, "function");
+        });
+
+        it("should return a string", function() {
+            assert.typeOf(iife.surround(""), "string");
+        });
+    });
+});
