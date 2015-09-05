6f77e35a3ff1a336e595119f1f3e3f62a72e2451
Marius Schulz <marius.schulz@me.com>
2015-09-05 13:22:35 +0200

    Starts with a blank slate for TDD

--- src/iife.js
+++ src/iife.js
@@ -2,28 +2,6 @@ import _ from "lodash";
 
 export default { surround };
 
-let defaultOptions = {
-    bindThis: false,
-    prependSemicolon: true,
-    useStrict: true,
-    trimCode: true
-};
-
 function surround(code, userOptions) {
-    let options = _.merge({}, defaultOptions, userOptions);
-
-    let bindThis = options.bindThis ? ".bind(this)" : "",
-        leadingCode = "(function() {\n",
-        trimmedCode = options.trimCode ? code.trim() : code,
-        trailingCode = "\n}" + bindThis + "());\n";
-
-    if (options.prependSemicolon) {
-       leadingCode = ";" + leadingCode;
-    }
-
-    if (options.useStrict && !code.match(/^\s*(['"])use strict\1;/)) {
-        leadingCode += '"use strict";\n\n';
-    }
-
-    return leadingCode + trimmedCode + trailingCode;
+    return code;
 }
