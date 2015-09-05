59a39280ee8749b8cf203242db4c19dd5cd2311c
Marius Schulz <marius.schulz@me.com>
2015-09-05 12:23:00 +0200

    Uses through2 for easier stream handling

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-var Stream = require("stream");
+var through = require("through2");
 var iife = require("./iife");
 
 function gulpIife(userOptions) {
     "use strict";
 
-    var stream = new Stream.Transform({ objectMode: true });
-
-    stream._transform = function(file, encoding, callback) {
+    return through.obj(function(file, encoding, callback) {
         var contents = String(file.contents);
         var wrappedContents = iife.surround(contents, userOptions);
 
         file.contents = Buffer(wrappedContents);
 
         callback(null, file);
-    };
-
-    return stream;
+    });
 }
 
 module.exports = gulpIife;
